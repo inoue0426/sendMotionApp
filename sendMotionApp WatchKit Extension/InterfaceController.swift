983cd2b86d4413951fe6ea43801d781c386cdccb
井上雄貴 <sekihan_tennis@yahoo.co.jp>
2017-11-30 18:48:33 +0900

    initial commit

---
+++ sendMotionApp WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,89 @@
+//
+//  InterfaceController.swift
+//  sendMotionApp WatchKit Extension
+//
+//  Created by 井上雄貴 on 2017/11/30.
+//  Copyright © 2017年 井上雄貴. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import WatchConnectivity
+import CoreMotion
+
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
+    @IBOutlet var label: WKInterfaceLabel!
+    
+    let motionManager = CMMotionManager()
+    let queue = OperationQueue()
+    
+    var attitude = ""
+    var gravity = ""
+    var rotationRate = ""
+    var userAcceleration = ""
+    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        activateSession()
+        
+        if !motionManager.isDeviceMotionAvailable {
+            print("Device Motion is not available.")
+            return
+        }
+        
+        motionManager.startDeviceMotionUpdates(to: queue) { (deviceMotion: CMDeviceMotion?, error: Error?) in
+            if error != nil {
+                print("Encountered error: \(error!)")
+            }
+            
+            if deviceMotion != nil {
+//                print("attitude = \(deviceMotion!.attitude)")
+//                print("gravity = \(deviceMotion!.gravity)")
+//                print("rotationRate = \(deviceMotion!.rotationRate)")
+//                print("userAcceleration = \(deviceMotion!.userAcceleration)")
+//
+                self.attitude = "\(deviceMotion!.attitude)"
+                self.gravity = "\(deviceMotion!.gravity)"
+                self.rotationRate = "\(deviceMotion!.rotationRate)"
+                self.userAcceleration = "\(deviceMotion!.userAcceleration)"
+                
+                self.sendMessage()
+            }
+        }
+    }
+    
+    func activateSession(){
+        if WCSession.isSupported() {
+            let session = WCSession.default
+            session.delegate = self
+            session.activate()
+            //            self.sendMessage()
+        }
+    }
+    
+    func sendMessage(){
+        if WCSession.default.isReachable {
+            let applicationDict = [
+                "attitude": attitude,
+                "gravity": gravity,
+                "rotationRate": rotationRate,
+                "userAcceleration": userAcceleration
+            ]
+            //            let applicationDict = ["test":"Hello, World!!"]
+            WCSession.default.sendMessage(applicationDict, replyHandler: {(reply) -> Void in
+                print(reply)
+            }){(error) -> Void in
+                print(error)
+            }
+        }
+    }
+    
+    @available(watchOS 2.2, *)
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        print("activationDidComplete")
+    }
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        //        print(message[0] as String)
+    }
+}
---
+++ sendMotionApp/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  sendMotionApp
+//
+//  Created by 井上雄貴 on 2017/11/30.
+//  Copyright © 2017年 井上雄貴. All rights reserved.
+//
+
+import UIKit
+import WatchConnectivity
+
+class ViewController: UIViewController, WCSessionDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        activateSession()
+    }
+
+    func activateSession(){
+        if WCSession.isSupported() {
+            let session = WCSession.default
+            session.delegate = self
+            session.activate()
+        }
+    }
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any], replyHandler: @escaping ([String : Any]) -> Void) {
+        
+        print(message["test"] as! String)
+        
+        replyHandler(["reply" : "OK"])
+    }
+    
+    @available(iOS 9.3, *)
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        print("activationDidComplete")
+    }
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        print("sessionDidBecomeInactive")
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        print("sessionDidDeactivate")
+    }
+}
