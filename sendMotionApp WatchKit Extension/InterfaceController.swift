2edf061e1bebfcec208344d79eaffbf95b503dc0
井上雄貴 <sekihan_tennis@yahoo.co.jp>
2017-12-05 22:00:46 +0900

    auto commit

--- sendMotionApp WatchKit Extension/InterfaceController.swift
+++ sendMotionApp WatchKit Extension/InterfaceController.swift
@@ -17,55 +17,37 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
     @IBOutlet var StopBtn: WKInterfaceButton!
     
     @IBAction func pushStartBtn() {
-        self.sendMessage()
         StartBtn.setHidden(true)
         StopBtn.setHidden(false)
+        print("start")
+        randNum = String(arc4random_uniform(10000000))
+        sendMessage()
     }
     
     @IBAction func pushStopBtn() {
+        motionManager.stopDeviceMotionUpdates()
+        print("stop")
         StartBtn.setHidden(false)
         StopBtn.setHidden(true)
-        motionManager.stopDeviceMotionUpdates()
+        randNum = ""
+        sendMessage()
     }
     
     let motionManager = CMMotionManager()
     let queue = OperationQueue()
     
+    var randNum =  String(UInt32())
     var applicationDict = [String: String]()
     var attitude = ""
     var gravity = ""
     var rotationRate = ""
     var userAcceleration = ""
-    var workout = ""
+    
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         activateSession()
         StopBtn.setHidden(true)
-        
-//        if !motionManager.isDeviceMotionAvailable {
-//            print("Device Motion is not available.")
-//            return
-//        }
-//
-//        motionManager.startDeviceMotionUpdates(to: queue) { (deviceMotion: CMDeviceMotion?, error: Error?) in
-//            if error != nil {
-//                print("Encountered error: \(error!)")
-//            }
-//
-//            if deviceMotion != nil {
-//                self.attitude = "\(deviceMotion!.attitude)"
-//                self.gravity = "\(deviceMotion!.gravity)"
-//                self.rotationRate = "\(deviceMotion!.rotationRate)"
-//                self.userAcceleration = "\(deviceMotion!.userAcceleration)"
-//
-////                print(self.attitude)
-////                print(self.gravity)
-////                print(self.rotationRate)
-////                print(self.userAcceleration)
-//            }
-//            sleep(UInt32(0.5))
-//        }
     }
     
     func activateSession(){
@@ -77,51 +59,45 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
     }
     
     func sendMessage(){
-//        let randNum = arc4random_uniform(10000000)
-        
-//        if !motionManager.isDeviceMotionAvailable {
-//            print("Device Motion is not available.")
-//            return
-//        }
-        
-        if WCSession.default.isReachable {
-            self.applicationDict = [
-                //                    "deviceLabel": "D\(randNum)",
-                //                    "attitude": self.attitude,
-                //                    "gravity": self.gravity,
-                //                    "rotationRate": self.rotationRate,
-                //                    "userAcceleration": self.userAcceleration
-                "test":"test"
-            ]
-            
-            WCSession.default.sendMessage(self.applicationDict, replyHandler: {(reply) -> Void in
-                print(reply)
-            }){(error) -> Void in
-                print(error)
-            }
+        if !motionManager.isDeviceMotionAvailable {
+            print("Device Motion is not available.")
+            return
         }
         
-//        motionManager.startDeviceMotionUpdates(to: queue) { (deviceMotion: CMDeviceMotion?, error: Error?) in
-//            if error != nil {
-//                print("Encountered error: \(error!)")
-//            }
-        
-//            if deviceMotion != nil {
-//                self.attitude = "\(deviceMotion!.attitude)"
-//                self.gravity = "\(deviceMotion!.gravity)"
-//                self.rotationRate = "\(deviceMotion!.rotationRate)"
-//                self.userAcceleration = "\(deviceMotion!.userAcceleration)"
-////
-////                                print(self.attitude)
-////                                print(self.gravity)
-////                                print(self.rotationRate)
-////                                print(self.userAcceleration)
-//            }
-            
-//            sleep(UInt32(0.5))
-            
-//        }
-//        print(applicationDict["deviceLabel"])
+        if self.randNum == "" {
+            motionManager.stopDeviceMotionUpdates()
+            return
+        }else{
+            motionManager.startDeviceMotionUpdates(to: queue) { (deviceMotion: CMDeviceMotion?, error: Error?) in
+                
+                if error != nil {
+                    print("Encountered error: \(error!)")
+                }
+                
+                if deviceMotion != nil {
+                    self.attitude = "\(deviceMotion!.attitude)"
+                    self.gravity = "\(deviceMotion!.gravity)"
+                    self.rotationRate = "\(deviceMotion!.rotationRate)"
+                    self.userAcceleration = "\(deviceMotion!.userAcceleration)"
+                }
+                
+                if WCSession.default.isReachable {
+                    self.applicationDict = [
+                        "deviceLabel": self.randNum,
+                        "attitude": self.attitude,
+                        "gravity": self.gravity,
+                        "rotationRate": self.rotationRate,
+                        "userAcceleration": self.userAcceleration
+                    ]
+                    sleep(UInt32(0.5))
+                    WCSession.default.sendMessage(self.applicationDict, replyHandler: {(reply) -> Void in
+                        print(reply)
+                    }){(error) -> Void in
+                        print(error)
+                    }
+                }
+            }
+        }
     }
     
     @available(watchOS 2.2, *)
@@ -130,7 +106,6 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
     }
     
     func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
-
     }
     
     
--- sendMotionApp/ViewController.swift
+++ sendMotionApp/ViewController.swift
@@ -24,9 +24,21 @@ class ViewController: UIViewController, WCSessionDelegate {
         }
     }
     
+    
+    
+    
+    
     func session(_ session: WCSession, didReceiveMessage message: [String : Any], replyHandler: @escaping ([String : Any]) -> Void) {
-        replyHandler(["reply" : message["OK"] as! String ])
+//        var request = URLRequest(url: NSURL(string: "https://script.google.com/macros/s/AKfycbx592MBeBoz8pygZESl7etv6ws3gfADMZxV4oGHRY7Z11GvEhV0/exec?attitude=\(String(describing: message["attitude"]))&gravity=\(String(describing: message["gravity"]))&rotationRate=\(String(describing: message["rotationRate"]))&userAcceleration=\(String(describing: message["userAcceleration"]))&time=22")! as URL)
+        
+        var request = URLRequest(url: NSURL(string: "https://script.google.com/macros/s/AKfycbx592MBeBoz8pygZESl7etv6ws3gfADMZxV4oGHRY7Z11GvEhV0/exec?attitude=10&gravity=20&rotationRate=30&userAcceleration=40&time=22")! as URL)
+        request.httpMethod = "GET"
+//        NSURLConnection.sendSynchronousRequest(request, returning: nil)
+        
+        replyHandler(["reply" : "test"])
     }
+    
+  
     
     @available(iOS 9.3, *)
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
