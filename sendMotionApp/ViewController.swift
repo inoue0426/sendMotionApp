d7981ff8006d2ec48751a73a0b9fe586f54e314a
井上雄貴 <sekihan_tennis@yahoo.co.jp>
2018-02-17 16:20:45 +0900

    auto commit

--- sendMotionApp/ViewController.swift
+++ sendMotionApp/ViewController.swift
@@ -31,8 +31,8 @@ class ViewController: UIViewController, WCSessionDelegate {
     func session(_ session: WCSession, didReceiveMessage message: [String : Any], replyHandler: @escaping ([String : Any]) -> Void) {
 //        var request = URLRequest(url: NSURL(string: "https://script.google.com/macros/s/AKfycbx592MBeBoz8pygZESl7etv6ws3gfADMZxV4oGHRY7Z11GvEhV0/exec?attitude=\(String(describing: message["attitude"]))&gravity=\(String(describing: message["gravity"]))&rotationRate=\(String(describing: message["rotationRate"]))&userAcceleration=\(String(describing: message["userAcceleration"]))&time=22")! as URL)
         
-        var request = URLRequest(url: NSURL(string: "https://script.google.com/macros/s/AKfycbx592MBeBoz8pygZESl7etv6ws3gfADMZxV4oGHRY7Z11GvEhV0/exec?attitude=10&gravity=20&rotationRate=30&userAcceleration=40&time=22")! as URL)
-        request.httpMethod = "GET"
+//        var request = URLRequest(url: NSURL(string: "https://script.google.com/macros/s/AKfycbx592MBeBoz8pygZESl7etv6ws3gfADMZxV4oGHRY7Z11GvEhV0/exec?attitude=10&gravity=20&rotationRate=30&userAcceleration=40&time=22")! as URL)
+//        request.httpMethod = "GET"
 //        NSURLConnection.sendSynchronousRequest(request, returning: nil)
         
         replyHandler(["reply" : "test"])
